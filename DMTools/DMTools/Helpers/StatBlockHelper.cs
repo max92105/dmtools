8e2f9e994f24551a3121fe66ae7e250505722edf
maxime.desgagné <maxime.desgane@tlmgo.com>
2026-05-06 11:51:26 -0400

    stats block

--- DMTools/DMTools/Helpers/StatBlockHelper.cs
+++ DMTools/DMTools/Helpers/StatBlockHelper.cs
@@ -58,6 +58,17 @@ namespace DMTools.Helpers
             return GenerateMonster(statBlockHelperDataModel.Monster);
         }
 
+        private static string FormatModifier(int score)
+        {
+            int mod = (score - 10) / 2;
+            return mod >= 0 ? "+" + mod : "\u2013" + Math.Abs(mod);
+        }
+
+        private static string AbilityText(int score)
+        {
+            return score + " (" + FormatModifier(score) + ")";
+        }
+
         public static String GenerateMonster(Monster monster)
         {
             StatBlockHelperDataModel monsterStats = StatBlockHelperDataController.LoadMonsterStats(monster.Id);
@@ -68,179 +79,166 @@ namespace DMTools.Helpers
 
             String statBlock = _StatHeader;
 
-            statBlock += @"<stat-block>";
-            statBlock += @"<creature-heading>";
-            statBlock += @"<h1>" + monster.Name + "</h1>";
-            statBlock += @"<h2>" + monster.Size + " " + monster.Type + " ";
+            statBlock += "<div class=\"stat-block\">";
+            statBlock += "<div class=\"bar\"></div>";
+            statBlock += "<div class=\"content-wrap\">";
+
+            // Creature Heading
+            statBlock += "<div class=\"creature-heading\">";
+            statBlock += "<h1>" + monster.Name + "</h1>";
+            statBlock += "<h2>" + monster.Size + " " + monster.Type + " ";
 
             if (!String.IsNullOrEmpty(monster.Subtype))
-                statBlock += @"(" + monster.Subtype + "), ";
+                statBlock += "(" + monster.Subtype + "), ";
 
             statBlock += monster.Alignment + "</h2>";
-            statBlock += @"</creature-heading>";
-            statBlock += @"<top-stats>";
-            statBlock += @"<property-line>";
-            statBlock += @"<h4>Armor Class </h4>";
-            statBlock += @"<p>";
-            statBlock += monster.ArmorClass;
-            statBlock += @"</p>";
-            statBlock += @"</property-line>";
-            statBlock += @"<property-line>";
-            statBlock += @"<h4>Hit Points </h4>";
-            statBlock += @"<p>" + monster.HitPoints + " (" + monster.HitDice + ")</p>";
-            statBlock += @"</property-line>";
-            statBlock += @"<property-line>";
-            statBlock += @"<h4>Speed </h4>";
-            statBlock += @"<p>" + monster.Speed + "</p>";
-            statBlock += @"<abilities-block data-cha=" + _StatBlockDataModel.Characteristics.First(obj => obj.CharacteristicTypeId == Characteristics.CharismaId).Score
-                + " data-con=" + _StatBlockDataModel.Characteristics.First(obj => obj.CharacteristicTypeId == Characteristics.ConstitutionId).Score
-                + " data-dex=" + _StatBlockDataModel.Characteristics.First(obj => obj.CharacteristicTypeId == Characteristics.DexterityId).Score
-                + " data-int=" + _StatBlockDataModel.Characteristics.First(obj => obj.CharacteristicTypeId == Characteristics.IntelligenceId).Score
-                + " data-str=" + _StatBlockDataModel.Characteristics.First(obj => obj.CharacteristicTypeId == Characteristics.StrenghtId).Score
-                + " data-wis=" + _StatBlockDataModel.Characteristics.First(obj => obj.CharacteristicTypeId == Characteristics.WisdomId).Score
-                + " ></abilities-block>";
+            statBlock += "</div>";
 
-            statBlock += @"<property-line>";
-            statBlock += @"<h4>Saving Throws </h4>";
-            statBlock += @"<p>";
+            // Top Stats
+            statBlock += "<div class=\"top-stats\">";
+            statBlock += "<hr class=\"tapered-rule\" />";
 
+            statBlock += "<div class=\"property-line\"><h4>Armor Class </h4><p>" + monster.ArmorClass + "</p></div>";
+            statBlock += "<div class=\"property-line\"><h4>Hit Points </h4><p>" + monster.HitPoints + " (" + monster.HitDice + ")</p></div>";
+            statBlock += "<div class=\"property-line\"><h4>Speed </h4><p>" + monster.Speed + "</p></div>";
+
+            statBlock += "<hr class=\"tapered-rule\" />";
+
+            // Abilities Table
+            var str = _StatBlockDataModel.Characteristics.FirstOrDefault(obj => obj.CharacteristicTypeId == Characteristics.StrenghtId);
+            var dex = _StatBlockDataModel.Characteristics.FirstOrDefault(obj => obj.CharacteristicTypeId == Characteristics.DexterityId);
+            var con = _StatBlockDataModel.Characteristics.FirstOrDefault(obj => obj.CharacteristicTypeId == Characteristics.ConstitutionId);
+            var intl = _StatBlockDataModel.Characteristics.FirstOrDefault(obj => obj.CharacteristicTypeId == Characteristics.IntelligenceId);
+            var wis = _StatBlockDataModel.Characteristics.FirstOrDefault(obj => obj.CharacteristicTypeId == Characteristics.WisdomId);
+            var cha = _StatBlockDataModel.Characteristics.FirstOrDefault(obj => obj.CharacteristicTypeId == Characteristics.CharismaId);
+
+            statBlock += "<table class=\"abilities-table\"><tbody>";
+            statBlock += "<tr><th>STR</th><th>DEX</th><th>CON</th><th>INT</th><th>WIS</th><th>CHA</th></tr>";
+            statBlock += "<tr>";
+            statBlock += "<td>" + (str != null ? AbilityText(str.Score) : "-") + "</td>";
+            statBlock += "<td>" + (dex != null ? AbilityText(dex.Score) : "-") + "</td>";
+            statBlock += "<td>" + (con != null ? AbilityText(con.Score) : "-") + "</td>";
+            statBlock += "<td>" + (intl != null ? AbilityText(intl.Score) : "-") + "</td>";
+            statBlock += "<td>" + (wis != null ? AbilityText(wis.Score) : "-") + "</td>";
+            statBlock += "<td>" + (cha != null ? AbilityText(cha.Score) : "-") + "</td>";
+            statBlock += "</tr></tbody></table>";
+
+            statBlock += "<hr class=\"tapered-rule\" />";
+
+            // Saving Throws
+            String savesText = "";
             foreach (CharacteristicType characteristicType in _StatBlockDataModel.CharacteristicTypes)
             {
-                Characteristic characteristic = _StatBlockDataModel.Characteristics.First(obj => obj.CharacteristicTypeId == characteristicType.Id);
+                Characteristic characteristic = _StatBlockDataModel.Characteristics.FirstOrDefault(obj => obj.CharacteristicTypeId == characteristicType.Id);
 
-                if (characteristic.Save != 0)
+                if (characteristic != null && characteristic.Save != 0)
                 {
-                    statBlock += characteristicType.Name + " " + characteristic.Save + " ";
+                    if (savesText.Length > 0) savesText += ", ";
+                    savesText += characteristicType.Name + " " + (characteristic.Save >= 0 ? "+" : "") + characteristic.Save;
                 }
             }
 
-            statBlock += @" </p>";
-            statBlock += @"</property-line>";
-
-            statBlock += @"<property-line>";
-            statBlock += @"<h4>Skills </h4>";
-            statBlock += @"<p>";
-
-            foreach (Skill skill in _StatBlockDataModel.Skills)
+            if (!String.IsNullOrEmpty(savesText))
             {
-                statBlock += _StatBlockDataModel.SkillTypes.First(obj => obj.Id == skill.SkillTypeId).Name + " " + skill.Save + " ";
+                statBlock += "<div class=\"property-line\"><h4>Saving Throws </h4><p>" + savesText + "</p></div>";
             }
 
-            statBlock += @" </p>";
-            statBlock += @"</property-line>";
-
-            if (!String.IsNullOrEmpty(monster.ConditionImmunities))
+            // Skills
+            if (_StatBlockDataModel.Skills.Count > 0)
             {
-                statBlock += @"<property-line>";
-                statBlock += @"<h4>Condition Immunities </h4>";
-                statBlock += @"<p>" + monster.ConditionImmunities + "</p>";
-                statBlock += @"</property-line>";
+                String skillsText = "";
+                foreach (Skill skill in _StatBlockDataModel.Skills)
+                {
+                    var skillType = _StatBlockDataModel.SkillTypes.FirstOrDefault(obj => obj.Id == skill.SkillTypeId);
+                    if (skillType != null)
+                    {
+                        if (skillsText.Length > 0) skillsText += ", ";
+                        skillsText += skillType.Name + " " + (skill.Save >= 0 ? "+" : "") + skill.Save;
+                    }
+                }
+                statBlock += "<div class=\"property-line\"><h4>Skills </h4><p>" + skillsText + "</p></div>";
             }
 
             if (!String.IsNullOrEmpty(monster.DamageVulnerabilities))
             {
-                statBlock += @"<property-line>";
-                statBlock += @"<h4>Damage Vulnerabilities </h4>";
-                statBlock += @"<p>" + monster.DamageVulnerabilities + "</p>";
-                statBlock += @"</property-line>";
+                statBlock += "<div class=\"property-line\"><h4>Damage Vulnerabilities </h4><p>" + monster.DamageVulnerabilities + "</p></div>";
             }
 
             if (!String.IsNullOrEmpty(monster.DamageResistances))
             {
-                statBlock += @"<property-line>";
-                statBlock += @"<h4>Damage Resistances </h4>";
-                statBlock += @"<p>" + monster.DamageResistances + "</p>";
-                statBlock += @"</property-line>";
+                statBlock += "<div class=\"property-line\"><h4>Damage Resistances </h4><p>" + monster.DamageResistances + "</p></div>";
             }
 
             if (!String.IsNullOrEmpty(monster.DamageImmunities))
             {
-                statBlock += @"<property-line>";
-                statBlock += @"<h4>Damage Immunities </h4>";
-                statBlock += @"<p>" + monster.DamageImmunities + "</p>";
-                statBlock += @"</property-line>";
+                statBlock += "<div class=\"property-line\"><h4>Damage Immunities </h4><p>" + monster.DamageImmunities + "</p></div>";
             }
 
-            statBlock += @"<property-line>";
-            statBlock += @"<h4>Senses </h4>";
-            statBlock += @"<p>" + monster.Senses + "</p>";
-            statBlock += @"</property-line>";
+            if (!String.IsNullOrEmpty(monster.ConditionImmunities))
+            {
+                statBlock += "<div class=\"property-line\"><h4>Condition Immunities </h4><p>" + monster.ConditionImmunities + "</p></div>";
+            }
 
-            statBlock += @"<property-line>";
-            statBlock += @"<h4>Languages </h4>";
-            statBlock += @"<p>" + monster.Languages + "</p>";
-            statBlock += @"</property-line>";
+            statBlock += "<div class=\"property-line\"><h4>Senses </h4><p>" + monster.Senses + "</p></div>";
+            statBlock += "<div class=\"property-line\"><h4>Languages </h4><p>" + monster.Languages + "</p></div>";
+            statBlock += "<div class=\"property-line\"><h4>Challenge </h4><p>" + monster.ChallengeRating + "</p></div>";
 
-            statBlock += @"<property-line>";
-            statBlock += @"<h4>Challenge </h4>";
-            statBlock += @"<p>" + monster.ChallengeRating + "</p>";
-            statBlock += @"</property-line>";
+            statBlock += "</div>"; // end top-stats
 
-            statBlock += @"</top-stats>";
-
+            // Special Abilities
             foreach (SpecialAbility specialAbility in _StatBlockDataModel.SpecialAbilities)
             {
-                statBlock += @"<property-block>";
-                statBlock += @"<h4>" + specialAbility.Name + " </h4>";
-                statBlock += @"<p>";
-                statBlock += specialAbility.Description;
-                statBlock += @"</p>";
-                statBlock += @"</property-block>";
+                statBlock += "<div class=\"property-block\"><h4>" + specialAbility.Name + ". </h4><p>" + specialAbility.Description + "</p></div>";
             }
 
-            if (_StatBlockDataModel.Actions.Where(obj => !obj.IsLegendary && !obj.IsBonus && !obj.IsReaction).Count() > 0)
-                statBlock += @"<h3>Actions</h3>";
-
-            foreach (Data.Objects.Action action in _StatBlockDataModel.Actions.Where(obj => !obj.IsLegendary && !obj.IsBonus && !obj.IsReaction))
+            // Actions
+            var normalActions = _StatBlockDataModel.Actions.Where(obj => !obj.IsLegendary && !obj.IsBonus && !obj.IsReaction).ToList();
+            if (normalActions.Count > 0)
             {
-                statBlock += @"<property-block>";
-                statBlock += @"<h4>" + action.Name + " </h4>";
-                statBlock += @"<p>";
-                statBlock += action.Description;
-                statBlock += @"</p>";
-                statBlock += @"</property-block>";
+                statBlock += "<h3 class=\"section-header\">Actions</h3>";
+                foreach (Data.Objects.Action action in normalActions)
+                {
+                    statBlock += "<div class=\"property-block\"><h4>" + action.Name + ". </h4><p>" + action.Description + "</p></div>";
+                }
             }
 
-            if (_StatBlockDataModel.Actions.Where(obj => obj.IsBonus).Count() > 0)
-                statBlock += @"<h3>Bonus Actions</h3>";
-
-            foreach (Data.Objects.Action action in _StatBlockDataModel.Actions.Where(obj => obj.IsBonus))
+            // Bonus Actions
+            var bonusActions = _StatBlockDataModel.Actions.Where(obj => obj.IsBonus).ToList();
+            if (bonusActions.Count > 0)
             {
-                statBlock += @"<property-block>";
-                statBlock += @"<h4>" + action.Name + " </h4>";
-                statBlock += @"<p>";
-                statBlock += action.Description;
-                statBlock += @"</p>";
-                statBlock += @"</property-block>";
+                statBlock += "<h3 class=\"section-header\">Bonus Actions</h3>";
+                foreach (Data.Objects.Action action in bonusActions)
+                {
+                    statBlock += "<div class=\"property-block\"><h4>" + action.Name + ". </h4><p>" + action.Description + "</p></div>";
+                }
             }
 
-            if (_StatBlockDataModel.Actions.Where(obj => obj.IsReaction).Count() > 0)
-                statBlock += @"<h3>Reactions</h3>";
-
-            foreach (Data.Objects.Action action in _StatBlockDataModel.Actions.Where(obj => obj.IsReaction))
+            // Reactions
+            var reactions = _StatBlockDataModel.Actions.Where(obj => obj.IsReaction).ToList();
+            if (reactions.Count > 0)
             {
-                statBlock += @"<property-block>";
-                statBlock += @"<h4>" + action.Name + " </h4>";
-                statBlock += @"<p>";
-                statBlock += action.Description;
-                statBlock += @"</p>";
-                statBlock += @"</property-block>";
+                statBlock += "<h3 class=\"section-header\">Reactions</h3>";
+                foreach (Data.Objects.Action action in reactions)
+                {
+                    statBlock += "<div class=\"property-block\"><h4>" + action.Name + ". </h4><p>" + action.Description + "</p></div>";
+                }
             }
 
-            if (_StatBlockDataModel.Actions.Where(obj => obj.IsLegendary).Count() > 0)
-                statBlock += @"<h3>Legendary Actions</h3>";
-
-            foreach (Data.Objects.Action action in _StatBlockDataModel.Actions.Where(obj => obj.IsLegendary))
+            // Legendary Actions
+            var legendaryActions = _StatBlockDataModel.Actions.Where(obj => obj.IsLegendary).ToList();
+            if (legendaryActions.Count > 0)
             {
-                statBlock += @"<property-block>";
-                statBlock += @"<h4>" + action.Name + " </h4>";
-                statBlock += @"<p>";
-                statBlock += action.Description;
-                statBlock += @"</p>";
-                statBlock += @"</property-block>";
+                statBlock += "<h3 class=\"section-header\">Legendary Actions</h3>";
+                foreach (Data.Objects.Action action in legendaryActions)
+                {
+                    statBlock += "<div class=\"property-block\"><h4>" + action.Name + ". </h4><p>" + action.Description + "</p></div>";
+                }
             }
 
-            statBlock += @" </stat-block> </body> </html>";
+            statBlock += "</div>"; // end content-wrap
+            statBlock += "<div class=\"bar\"></div>";
+            statBlock += "</div>"; // end stat-block
+            statBlock += "</body></html>";
 
             return statBlock;
         }
